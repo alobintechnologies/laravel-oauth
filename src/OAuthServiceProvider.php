15a84b8e4c6168b559a8d6b364ae5315787c2a47
Scott Wilcox <scott@dor.ky>
2014-07-06 22:31:07 +0100

    Moved the Eloquent storage implementation into this package Adjusted service provider to load this class instead of the testing class I used previously

--- src/OAuthServiceProvider.php
+++ src/OAuthServiceProvider.php
@@ -29,7 +29,7 @@ class OAuthServiceProvider extends ServiceProvider {
             $factory = new ServiceFactory;
 
             $session = $app->make('session')->driver();
-            $storage = new \OAuth\Common\Storage\Eloquent($session);
+            $storage = new \SSX\OAuth\Storage\Eloquent($session);
 
             return new OAuth($factory, $storage);
         });
